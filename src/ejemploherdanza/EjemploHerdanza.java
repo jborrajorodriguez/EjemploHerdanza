8f6bca126ce5226deeb5b0682c5385e69495a232
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-22 09:39:35 +0100

    Clase main

---
+++ src/ejemploherdanza/EjemploHerdanza.java
@@ -0,0 +1,13 @@
+package ejemploherdanza;
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class EjemploHerdanza {
+
+    public static void main(String[] args) {
+        Estudante alumno1=new Estudante("Juan","Borrajo",22,"5937",10);
+        alumno1.amosar();
+
+    }
+
+}
